547e86a848577d066ed0c0e522df1ad3550c8177
DianaHuaripayta <angelicagonzales1610@gmail.com>
2021-12-01 13:19:31 -0500

    start with add sidebar

--- src/app/app.js
+++ src/app/app.js
@@ -4,9 +4,12 @@ const closeMenuBtn = document.querySelector(".close-menu");
 
 function toggleMenu() {
   menu.classList.toggle("menu_opened");
-
 }
-
-openMenuBtn.addEventListener("click", toggleMenu);//add , setTimeout(myFunc, 2000)
-
-closeMenuBtn.addEventListener("click", toggleMenu);//quitar
+openMenuBtn.addEventListener("click", toggleMenu);
+closeMenuBtn.addEventListener("click", toggleMenu);
+/* cerrar */
+const navLink = document.querySelectorAll(".nav-item");
+navLink.forEach(item => item.addEventListener('click', linkClose))
+function linkClose() {
+  menu.classList.remove("menu_opened");
+}
